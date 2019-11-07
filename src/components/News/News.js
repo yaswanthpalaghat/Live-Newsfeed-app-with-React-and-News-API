93fe0d30e132522b0f733ce10fdddd3bb9a4b8c5
yash <yash@gmail>
2019-11-07 15:46:45 +0530

    react news

---
+++ src/components/News/News.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import NewSingle from './NewSingle';
+import Error from './Error';
+
+class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: [],
+      error: false,
+    };
+  }
+
+  componentDidMount() {
+    const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=5b2db456d8d241e19007e819f9097712`;
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          news: data.articles
+        })
+      })
+      .catch((error) => {
+        this.setState({
+          error: true
+        })
+      });
+  }
+
+  renderItems() {
+    if (!this.state.error) {
+      return this.state.news.map((item) => (
+        <NewSingle key={item.url} item={item} />
+      ));
+    } else {
+      return <Error />
+    }
+  }
+
+  render() {
+    return (
+      <div className="row">
+        {this.renderItems()}
+      </div>
+    );
+  }
+}
+
+export default News;
